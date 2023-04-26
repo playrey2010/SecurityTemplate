2356f8617ac24df049bcdf43b6e8bb1375614c4e
playrey2010 <reynaldo.emanuel@gmail.com>
2023-04-26 16:16:36 -0700

    working logging page with demo users

---
+++ src/main/java/com/example/springdemosecurity2023/SpringDemoSecurity2023Application.java
@@ -0,0 +1,13 @@
+package com.example.springdemosecurity2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoSecurity2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoSecurity2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdemosecurity2023/SpringDemoSecurity2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdemosecurity2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoSecurity2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
